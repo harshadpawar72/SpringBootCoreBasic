534d884ae566f6e2cc4efe502746579f868e84b3
Harshad Pawar <harshadpawar072@gmail.com>
2022-03-23 16:42:21 +0530

    added comments

--- SpringBootGitCore/src/main/java/com/csi/controller/ApplicationController.java
+++ SpringBootGitCore/src/main/java/com/csi/controller/ApplicationController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.LinkedList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class ApplicationController {
@@ -23,4 +26,41 @@ public class ApplicationController {
     {
         return "AKSHAY PARDESHI..!";
     }
+
+    @GetMapping("/showData")
+    public List<Employee> showData()
+    {
+        List<Employee> empList=new LinkedList<>();
+        empList.add(new Employee(555, "Harshad"));
+        empList.add(new Employee(456, "Ram"));
+        return empList;
+    }
+}
+class Employee
+{
+    private int empId;
+    private String empName;
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public Employee(int empId, String empName) {
+        this.empId = empId;
+        this.empName = empName;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "empId=" + empId +
+                ", empName='" + empName + '\'' +
+                '}';
+    }
+
+    // do some code here- from Harshad
 }
